4d03f29d4caba3aa4f1804beb5113519aa14426a
MMolieleng <mjmolieleng@gmail.com>
2024-09-17 18:14:36 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 from fastapi import FastAPI
-
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
 from llm import LLM
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-
-
 app = FastAPI()
-
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_ollama.llms import OllamaLLM
-
-template = """Question: {question}
-
-Answer: Let's think step by step."""
-
-prompt = ChatPromptTemplate.from_template(template)
-model = OllamaLLM(model="phi3:mini", verbose=True)
-chain = prompt | model
-
 llm = LLM()
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -37,5 +17,5 @@ async def test(name: str):
     return {"message": "Welcome to "+ name}
 
 @app.get("/chat/{message}")
-async def chat(message: str):
-    return {"response": chain.invoke({"question": message})}
+async def chat(question: str):
+    return {"response": llm.ask(question)}
